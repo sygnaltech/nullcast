a3d37d4b5982b1476bb37c9bad0172264cbec2f9
Michael Wells <mike@sygnal.com>
2026-02-07 17:45:09 +1300

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace VideoPlayer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
